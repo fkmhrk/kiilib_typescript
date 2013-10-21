546d368e7f981cfd21be0abafca41df8e796ddd6
hrk fkm <fkm@fkmsoft.jp>
2013-10-21 19:08:24 +0900

    fix build script for jquery

--- src/build_jquery.sh
+++ src/build_jquery.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+
+# copy context file
+cp KiiContext_jquery.ts KiiContext.ts 
+
 tsc KiiContext.ts KiiUser.ts \
 kii/KiiAppAPI.ts \
---out KiiLib_base.js 
+--out KiiLib_base.js \
+-d
 
 # remove var kii;
 sed -e "s/var Kii;//g" KiiLib_base.js > tmp.js 
 cat header.js tmp.js > KiiLib.js
 
+# rename
+mv KiiLib_base.d.ts KiiLib.d.ts
