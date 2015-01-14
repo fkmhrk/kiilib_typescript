517f3b4b58fe68c93a673fb764c10d1794d7fc9e
hrk fkm <fkm@fkmsoft.jp>
2015-01-14 15:06:39 +0900

    Add blocking API to ACL API

--- src/ACLAPI.ts
+++ src/ACLAPI.ts
@@ -1,7 +1,9 @@
 /// <reference path="AppAPI.ts"/>
 module Kii {
     export interface ACLAPI {
+        grant(target : any, verb : string, subject : any);
 	grant(target : any, verb : string, subject : any, callback : KiiCallback);
+        revoke(target : any, verb : string, subject : any);
 	revoke(target : any, verb : string, subject : any, callback : KiiCallback);
     }
 }
